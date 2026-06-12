53524402262d95a57cd35be64049bc83aaa335a0
Bekaurielene <128158230+Bekaurielene@users.noreply.github.com>
2026-06-12 23:08:07 +0400

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,51 @@
+import H1 from "./H1";
+import Input from "./Input";
+import Paragraph from "./Paragraph";
+
+function App() {
+  let number = 10;
+
+  let arr = ["Apple", "Banana", "Orange"];
+
+  let str = "Hello React";
+
+  let obj = {
+    name: "Ele",
+    age: 25,
+  };
+
+  function fnc() {
+    return "Function Result";
+  }
+
+  return (
+    <>
+      <H1 />
+      <Input />
+      <Paragraph />
+
+      <h2>რა არის კომპონენტი?</h2>
+
+      <p>
+        კომპონენტი არის React-ში კოდის დამოუკიდებელი და მრავალჯერ გამოყენებადი
+        ნაწილი, რომელიც აბრუნებს JSX-ს.
+      </p>
+
+      <h2>მონაცემების გამოჩენა</h2>
+
+      <p>Number: {number}</p>
+
+      <p>String: {str}</p>
+
+      <p>Function: {fnc()}</p>
+
+      <p>Array: {arr.join(", ")}</p>
+
+      <p>Name: {obj.name}</p>
+
+      <p>Age: {obj.age}</p>
+    </>
+  );
+}
+
+export default App;
